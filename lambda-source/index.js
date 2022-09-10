5e40f142b813b8751a6016902afc85eda4b0b8b8
Tydragon00 <timothydrago@gmail.com>
2022-09-10 14:13:23 +0200

    add pipeline

--- lambda-source/index.js
+++ lambda-source/index.js
@@ -1,5 +1,5 @@
 exports.handler = async function (event, context) {
-    console.log("hello world from lambda of ty")
+    console.log("hello GITHUB ACTION!!!!!!!!! from lambda of ty")
     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     return context.logStreamName
 }
