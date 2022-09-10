1356957743c361f6acbae6a79fc810415d895918
Tydragon00 <timothydrago@gmail.com>
2022-09-10 14:15:24 +0200

    try

--- lambda-source/index.js
+++ lambda-source/index.js
@@ -1,5 +1,6 @@
 exports.handler = async function (event, context) {
     console.log("hello GITHUB ACTION!!!!!!!!! from lambda of ty")
+    //test
     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     return context.logStreamName
 }
