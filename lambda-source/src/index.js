eac37a725767f381613540acf19143be44374dff
Tydragon00 <timothydrago@gmail.com>
2022-09-10 18:31:45 +0200

    init

--- lambda-source/src/index.js
+++ lambda-source/src/index.js
@@ -1,5 +1,5 @@
 exports.handler = async function (event, context) {
-    console.log("hello GITHUB ACTION!!!!!!!!! from lambda of ty")
+    console.log("hello GITHUB test dynamodb!!!!!!!!! from lambda of ty")
     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     return context.logStreamName
 }
