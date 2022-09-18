8d0be66a54de80dc0be6db41231f2d4d542e8402
Tydragon00 <timothydrago@gmail.com>
2022-09-18 13:48:42 +0200

    test response

--- lambda-source/src/index.js
+++ lambda-source/src/index.js
@@ -1,5 +1,8 @@
 exports.handler = async function (event, context) {
-    console.log("hello GITHUB test dynamodb!!!!!!!!! from lambda of ty")
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    return context.logStreamName
+    let jsonResponse = { "hello": "world" }
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(jsonResponse),
+    };
+    return response;
 }
