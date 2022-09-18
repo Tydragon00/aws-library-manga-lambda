70aeb7337cb35595eaf4995ad39da19f41ccdb33
Tydragon00 <timothydrago@gmail.com>
2022-09-18 17:40:03 +0200

    add dyanamo db table

--- lambda-source/src/endpoints.js
+++ lambda-source/src/endpoints.js
@@ -7,4 +7,19 @@ module.exports = (api, opts) => {
         return responseBody;
 
     });
+    api.get('/library-manga/all', async (req, res) => {
+        let params = {
+            TableName: "library-manga-db"
+        };
+        try {
+            data = await documentClient.scan(params).promise();
+            let mangas = data.Items;
+            responseBody = JSON.stringify(mangas);
+            console.log({ data })
+        } catch (err) {
+            res.status(503).send(`Unable to get manga: ${err}`);
+        }
+        return responseBody;
+
+    });
 }
